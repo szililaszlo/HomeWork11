ba78750ab0f186988ce334b5f81492bafa9e5cf6
szililaszlo <laszlo0919@gmail.com>
2018-01-14 17:51:44 +0100

    #4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
         //3
-        Util.printYoutubeJSON("C:\\Users\\László\\IdeaProjects\\HomeWork11\\src\\main\\resources\\youtube.json");
+     //   Util.printYoutubeJSON("C:\\Users\\László\\IdeaProjects\\HomeWork11\\src\\main\\resources\\youtube.json");
+        //4
+        Util.convertYoutubeJSONToXML("C:\\Users\\László\\IdeaProjects\\HomeWork11\\src\\main\\resources\\youtube.json","C:\\Users\\László\\IdeaProjects\\HomeWork11\\src\\main\\resources\\youtube.xml");
     }
 
 }
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,7 +1,8 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
+import org.json.XML;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class Util {
 
@@ -9,14 +10,57 @@ public class Util {
     public static void printYoutubeJSON(String destination) {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            Youtube youtube = objectMapper.readValue(new File(destination),Youtube.class);
+            Youtube youtube = objectMapper.readValue(new File(destination), Youtube.class);
             System.out.println(youtube);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        catch (Exception e) {
+    }
+
+    //4
+    public static void convertYoutubeJSONToXML(String JSONFile, String XMLFile) {
+
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        try {
+            fin = new FileInputStream(JSONFile);
+            fout = new FileOutputStream(XMLFile);
+
+            int i;
+            String JSONTOString = "";
+            while ((i = fin.read()) !=-1) {
+                //hozzáírja a i-edik charactert a változóba
+                JSONTOString+= (char)i;
+            }
+
+            //a JSON objektum létrehozása
+            JSONObject json = new JSONObject(JSONTOString);
+            //konvertálás xml-be
+            String xml = XML.toString(json);
+            //fájlba írás
+            fout.write(xml.getBytes());
+        } catch (IOException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fout != null) {
+                try {
+                    fout.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fin != null) {
+                try {
+                    fin.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
